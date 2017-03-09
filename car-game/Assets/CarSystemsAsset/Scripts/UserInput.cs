d0e3b1a4e06547d2dcd9ea72cf32c054ab5a1d85
Scott Troutman <troutmansm@gmail.com>
2017-03-08 21:20:04 -0600

    models for pieces, better car

---
+++ car-game/Assets/CarSystemsAsset/Scripts/UserInput.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+
+public class UserInput : MonoBehaviour
+{
+    private CarControl m_Car; 
+
+    float h;
+    float accel;
+    float brake;
+    float handbrake;
+    float revears;
+    bool toggleDriftSystem;
+
+
+   const string Horizontal_KEY = "Horizontal";
+    const string Vertical_KEY = "Drive";
+    const string NITRO_KEY = "Boost";
+    const string HANDBRAKE_KEY = "Brake";
+    //const string DRIFT_SYSTEM_KEY = KeyCode.A;
+    bool activeNitro;
+
+    
+    void Start()
+    {
+    
+        m_Car = GetComponent<CarControl>();
+    }
+
+    void Update()
+    {
+        toggleDriftSystem = Input.GetKeyUp(KeyCode.A);
+        if (toggleDriftSystem)
+            m_Car.toggleDriftSystem();
+    }
+
+ 
+
+    void FixedUpdate()
+    {
+
+       
+         h =  Input.GetAxis(Horizontal_KEY) ;
+      
+        accel = brake = Input.GetAxis(Vertical_KEY) ;
+        handbrake = Input.GetAxis(HANDBRAKE_KEY);
+
+        m_Car.Move(h, accel, brake, handbrake); // pass the input to the car!
+        activeNitro = Input.GetButton(NITRO_KEY);
+        m_Car.nitro(activeNitro);
+
+       
+    }
+   
+}
+
